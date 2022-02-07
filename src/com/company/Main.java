0d0600db61d171a51e43fa4c21a0c592e975077d
Muhammed <muhammedtoichubai@gmail.com>
2022-02-07 00:20:43 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,87 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Konsolgo tuulgan jylynyzdy jazynyz");
+//        int a = scanner.nextInt();
+//        System.out.println("Men " + " " + a + "" +" jyly tuulgam");
+//        Scanner scanner1 = new Scanner(System.in);
+//        System.out.println("Konsolgo tuulgan jylynyzdy kaira jazynyz");
+//        int b = scanner1.nextInt();
+//        int   eki , result;
+//        eki = 2022;
+//        result = eki - b ;
+//        System.out.println("Uchurda " + " " + result+" "+ "jashtamyn" );
+//
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Kush keldiniz 'Peaksoft' magazinge.");
+//        System.out.println("Kurut sht.?");
+//        int m = scanner.nextInt();
+//        int kurut, shokalad, sagyz, summa, kaitymy;
+//        kurut = 5;
+//        shokalad = 45;
+//        sagyz = 5;
+//        System.out.println("1 daana kurut 5som" );
+//        Scanner scanner1 = new Scanner(System.in);
+//        System.out.println("Shokolad sht.?.");
+//        int u = scanner1.nextInt();
+//        System.out.println("1 daana shokalad 45som" );
+//        Scanner scanner2 = new Scanner(System.in);
+//        System.out.println("Sagyz sht.?.");
+//        int h = scanner2.nextInt();
+//        System.out.println("1 daana sagyz 5som" );
+//        summa = kurut + shokalad + sagyz;
+//        System.out.println("Sumanyz -"+" " +summa + ""+ " " + "som");
+//        System.out.println("Siz bergen akchanyzdyn summasy?");
+//        int a = scanner1.nextInt();
+//        kaitymy = a - summa;
+//        System.out.println("Kaitymynyz-" +kaitymy);
+
+//        Scanner peaksoft = new Scanner(System.in);
+//        double som,rub,$,tenge,euro;
+//        som = peaksoft.nextDouble();
+//               rub = som*1.13;
+//               $ = som*84.80;
+//               tenge = som*5.15;
+//               euro = som*96.07;
+//        System.out.printf("\tcourse for today: \nRub= %f\n$%f\ntenge %f\nevro %f",rub,$,tenge,euro);
+
+//        Scanner num = new Scanner(System.in);
+//        float first, second, third, result;
+//        System.out.println("Enter first num:");
+//        first = num.nextFloat();
+//        System.out.println("Enter second num:");
+//        second = num.nextFloat();
+//        System.out.println("Enter third num:");
+//        third = num.nextFloat();
+//        result = first+ second+ third;
+//        System.out.println("Result is - " + result);
+//        result = first* second* third;
+//        System.out.println("Result is - " + result);
+//        result = first+ second+ third;
+//        result = result / 3 ;
+//        System.out.println("Result is - " + result);
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
